35564f9e2baf0cebe1d27e6701e0739ede067bf8
ikaros <ikaros@localhost.localdomain>
2023-06-12 20:40:20 -0500

    2nd stage: make sure to add a 'b'

--- app/main.py
+++ app/main.py
@@ -9,7 +9,18 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+
+    with conn:
+        print(f"Connected by {addr}")
+
+        while True:
+            data = b"+PONG\r\n"
+
+            if not data:
+                break
+
+            conn.sendall(data)
 
 
 if __name__ == "__main__":
