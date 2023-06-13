d94752476200b92b32108809cb55c22f6d9155f1
ikaros <ikaros@localhost.localdomain>
2023-06-13 16:57:17 -0500

    added for loop to handle more pings

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    conn.recv(1024)
-    conn.send(b"+PONG\r\n")
+
+    while True:
+        conn.recv(1024)
+        conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
