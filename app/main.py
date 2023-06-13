6b3dd897291d9c78a28ea4f02dceb4371ee86039
ikaros <ikaros@localhost.localdomain>
2023-06-12 20:49:29 -0500

    2nd stage: just pushed using send not send all

--- app/main.py
+++ app/main.py
@@ -10,17 +10,8 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-
-    with conn:
-        print(f"Connected by {addr}")
-
-        while True:
-            data = b"+PONG\r\n"
-
-            if not data:
-                break
-
-            conn.sendall(data)
+    conn.recv(1024)
+    conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
