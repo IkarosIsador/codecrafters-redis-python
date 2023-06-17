af0876844d312a49fbbea929a8b3a27a2ba26c04
ikaros <ikaros@localhost.localdomain>
2023-06-16 19:49:15 -0500

    figured out adding the ECHO command

--- app/main.py
+++ app/main.py
@@ -1,22 +1,55 @@
 import asyncio
 
 
+def encode_bulk_string(values):
+    encoded_vals = []
+
+    for val in values:
+        encoded_value = f"${len(val)}\r\n{val}\r\n".encode()
+        encoded_vals.append(encoded_value)
+    return b"".join(encoded_vals)
+
+
+async def read_resp_array(header, reader):
+    num_items = int(header[1:])
+
+    array = []
+    for i in range(num_items):
+        item_header = await reader.readline()
+        item_size = int(item_header[1:])
+        item_data = await reader.readexactly(item_size + 2)
+        item = item_data[:-2].decode()
+        array.append(item)
+    return array
+
+
 async def handle_clients(reader, writer):
-    try:
-        while True:
-            data = await reader.read(256)
-            if not data:
+    while True:
+        try:
+
+            command = await reader.readline()
+            if not command:
                 break
 
-            print(b'+PONG\r\n')
-            writer.write(b'+PONG\r\n')
-            await writer.drain()
+            if command[0] == ord(b'*'):
+                array = await read_resp_array(command, reader)
 
-    except Exception as e:
-        print(f"Error in handling client: {e}")
-    finally:
-        writer.close()
-        await writer.wait_closed()
+            if array[0] == "PING":
+                writer.write(b"+PONG\r\n")
+                await writer.drain()
+            elif array[0] == "ECHO":
+                response = encode_bulk_string(array[1:])
+                writer.write(response)
+                await writer.drain()
+            writer.close()
+
+        except Exception as e:
+            print(command)
+            print(f"Error in handling client: {e}")
+        finally:
+            writer.close()
+            await writer.wait_closed()
+
 
 async def start_server():
     server = await asyncio.start_server(handle_clients, "localhost", 6379)
